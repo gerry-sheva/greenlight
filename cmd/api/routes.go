1c99115145caaab5a18faffe224cc22f695a498d
gerry-sheva <gerry.sheva@proton.me>
2024-04-16 07:51:16 +0700

    added 404 and 405 handler

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,9 +1,22 @@
 package main
 
-import "github.com/go-chi/chi/v5"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
 
 func (app *appliction) routes() *chi.Mux {
 	router := chi.NewRouter()
+
+	router.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(404)
+		w.Write([]byte("route does not exist"))
+	})
+	router.MethodNotAllowed(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(405)
+		w.Write([]byte("method is not valid"))
+	})
 
 	router.Get("/v1/healthcheck", app.healthCheckHandler)
 	router.Get("/v1/movies/{id}", app.showMovieHandler)
